e4498ce0401f7ddc83f701841638f3c3fae8d4fb
Experio2705 <aryanshinde2705@gmail.com>
2026-02-18 22:09:05 +0530

    Footer Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom"
-import Home from './pages/Home'
+import Home from "./pages/Home"
 import Product from "./pages/Product"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
@@ -7,16 +7,17 @@ import "./App.css"
 
 export default function App() {
   return (
-  <>
-    <Navbar />
-    <div className="container">
-     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:id" element={<Product />} />
-      </Routes>
-      </div>
-    <Footer />
-    </>
+    <div className="app">
+      <Navbar />
 
+      <main className="main container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<Product />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
   )
 }
